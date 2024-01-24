add03f44321efe38079285701b5a65527613b3aa
Tim Martin <tim.martin@gettyimages.com>
2024-01-24 09:22:47 -0700

    Catppuccin changes.

---
+++ scripts/pomodoro.sh
@@ -0,0 +1,9 @@
+show_pomodoro() {
+  local index=$1
+  local icon=$(get_tmux_option "@catppuccin_pomodoro_icon" "󰔛")
+  local color=$(get_tmux_option "@catppuccin_pomodoro_color" "$thm_red")
+  local text=$(get_tmux_option "@catppuccin_pomodoro_text" "#{pomodoro_status}")
+  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+
+  echo "$module"
+}
