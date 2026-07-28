1ca304935dbe9c5714975cf1f430025c81ed69d6
Tim Martin <tim.martin@gettyimages.com>
2026-07-28 09:20:34 -0600

    Snacks finder.

--- nvim-lua/.config/nvim/lua/plugins/snacks.lua
+++ nvim-lua/.config/nvim/lua/plugins/snacks.lua
@@ -1,62 +1,72 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      hidden = true,
-      ignored = true,
-      sources = {
-        explorer = {
-          hidden = true,
-          ignored = true,
-        },
-      },
-    },
-    explorer = {},
-  },
-  keys = {
-    -- Grep
-    {
-      "<leader>sb",
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = "Buffer Lines",
-    },
-    {
-      "<leader>sB",
-      function()
-        Snacks.picker.grep_buffers()
-      end,
-      desc = "Grep Open Buffers",
-    },
-    {
-      "<leader>sg",
-      function()
-        Snacks.picker.grep()
-      end,
-      desc = "Grep",
-    },
-    {
-      "<leader>sw",
-      function()
-        Snacks.picker.grep_word()
-      end,
-      desc = "Visual selection or word",
-      mode = { "n", "x" },
-    },
-    {
-      "<C-n>",
-      function()
-        Snacks.explorer()
-      end,
-      desc = "Show explorer",
-    },
-    {
-      "<C-p>",
-      function()
-        Snacks.picker.grep()
-      end,
-      desc = "Grep",
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		picker = {
+			hidden = true,
+			ignored = true,
+			exclude = {
+				"node_modules",
+				".git",
+				"dist",
+				"build",
+			},
+			sources = {
+				explorer = {
+					hidden = true,
+					ignored = true,
+					exclude = {
+						"node_modules",
+						".git",
+					},
+				},
+			},
+		},
+		explorer = {},
+	},
+	keys = {
+		-- Grep
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "Buffer Lines",
+		},
+		{
+			"<leader>sB",
+			function()
+				Snacks.picker.grep_buffers()
+			end,
+			desc = "Grep Open Buffers",
+		},
+		{
+			"<leader>sg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Visual selection or word",
+			mode = { "n", "x" },
+		},
+		{
+			"<C-n>",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Show explorer",
+		},
+		{
+			"<C-p>",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+	},
 }
